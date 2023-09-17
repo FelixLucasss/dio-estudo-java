4ff67d4ba60ea8dcd261e7157938dfe92e9f0819
Grymmur <felixlucas246@gmail.com>
2023-09-17 16:12:28 -0300

    java

---
+++ BootCamp Java/anatomia-de-classes/src/MinhaClasse.java
@@ -0,0 +1,14 @@
+
+public class MinhaClasse{
+    public static void main (String [] args) {
+      
+        String primeiroNome = "Lucas";
+        String segundoNome = "Felix";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome); 
+
+        System.out.print(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método = " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
