2a70bf7f358692486bbcce7e790d15aba5658857
Grymmur <felixlucas246@gmail.com>
2023-10-22 15:55:15 -0300

    Att

---
+++ BootCamp Java/lab/src/dio/facade/Facade.java
@@ -0,0 +1,13 @@
+package dio.facade;
+
+import sistema1.CepApi;
+import sistema2.CrmService;
+
+public class Facade {
+    public void migrarCliente(String nome, String cep) {
+        String cidade = CepApi.getInstancia().recuperarCidade(cep);
+        String estado = CepApi.getInstancia().recuperarEstado(cep);
+
+        CrmService.gravarCliente(nome, cep, cidade, estado);
+    }
+}
